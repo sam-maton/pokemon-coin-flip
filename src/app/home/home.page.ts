5df0878506ded836f614ae431798ba1d7f1e68e9
Sam Maton <sam.maton91@gmail.com>
2025-09-16 21:58:57 +0100

    started work on pokeball svg

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonRange } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonRange,
+  RangeCustomEvent,
+  IonText,
+  IonButton,
+  IonRippleEffect,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRange],
+  imports: [
+    IonRippleEffect,
+    IonButton,
+    IonText,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonRange,
+  ],
 })
 export class HomePage {
   constructor() {}
+
+  selectedValue: number = 1;
+
+  rangeChange(event: RangeCustomEvent) {
+    this.selectedValue = event.detail.value as number;
+  }
 }
